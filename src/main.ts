486cddf2c8d1bf8595cedfe3eb0d0cb8fc678754
Maxime Kiniffo <maxime.kiniffo-dev@proton.me>
2026-04-29 14:46:04 +0200

    first commit

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+import router from './router'
+import vuetify from './plugins/vuetify'
+import App from './App.vue'
+
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
+
+createApp(App)
+  .use(pinia)
+  .use(router)
+  .use(vuetify)
+  .mount('#app')
