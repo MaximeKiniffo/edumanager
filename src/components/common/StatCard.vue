5d9a51817ca812ba974d734a7dfd6d56fe4b4af0
Maxime Kiniffo <maxime.kiniffo-dev@proton.me>
2026-06-01 21:38:57 +0200

    add: statcard compnent and dashboard view started

---
+++ src/components/common/StatCard.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-card rounded="lg" :color="color" variant="tonal">
+    <h3>{{ title }}</h3>
+    <p>{{ value }}</p>
+    <i :class="icon" :style="{ color }"></i>
+  </v-card>
+</template>
+
+<script setup lang="ts">
+
+defineProps<{
+  title: string;
+  value: number;
+  icon: string;
+  color: string;
+}>();
+
+</script>
